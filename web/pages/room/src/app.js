b40e9390957b19201ae0d78378ddf4131b18dba0
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-25 20:58:15 -0300

    feat: add SocketBuilder to build the socket.io instance

--- web/pages/room/src/app.js
+++ web/pages/room/src/app.js
@@ -1,5 +1,3 @@
-
-
 const recordClick = function (recorderBtn) {
   this.recordingEnabled = false
   return () => {
@@ -9,13 +7,26 @@ const recordClick = function (recorderBtn) {
 }
 
 const onload = () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const room = urlParams.get('room');
+  const urlParams = new URLSearchParams(window.location.search)
+  const room = urlParams.get('room')
   console.log('this is the room', room)
 
-  const recorderBtn = document.getElementById('record')
-  recorderBtn.addEventListener('click', recordClick(recorderBtn))
+  // const recorderBtn = document.getElementById('record')
+  // recorderBtn.addEventListener('click', recordClick(recorderBtn))
 
+  const socketUrl = 'http://localhost:3000'
+  const socketBuilder = new SocketBuilder({ socketUrl })
+
+  const view = new View()
+  const media = new Media()
+  const deps = {
+    view,
+    media,
+    room,
+    socketBuilder,
+  }
+
+  Business.initialize(deps)
 }
 
 window.onload = onload
